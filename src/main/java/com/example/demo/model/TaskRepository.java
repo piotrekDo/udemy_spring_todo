bf667bc5355b5708678a04208eac9caff409eaf7
Piotrek Domagalski <piotr.domagalski@yahoo.com>
2023-01-27 15:46:40 +0100

    repository reworked

--- src/main/java/com/example/demo/model/TaskRepository.java
+++ src/main/java/com/example/demo/model/TaskRepository.java
@@ -1,13 +1,21 @@
 package com.example.demo.model;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
-@Repository
-public interface TaskRepository extends JpaRepository<Task, Integer> {
+public interface TaskRepository {
+
+    List<Task> findAll();
+
+    Optional<Task> findById(Integer id);
+
+    Task save(Task entity);
+
+    Page<Task> findAll(Pageable page);
 
     List<Task> findAllByDone(@Param("state") boolean isDone);
 }
