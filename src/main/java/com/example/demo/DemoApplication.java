d09121dc206b6a7d4831d4770cae5c7972521ef7
Piotrek Domagalski <piotr.domagalski@yahoo.com>
2023-01-27 11:58:11 +0100

    init commit for spring config section

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,13 @@
 package com.example.demo;
 
+import jakarta.validation.Validator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+
+
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +16,8 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
     }
 
+    @Bean
+    Validator validator() {
+        return new LocalValidatorFactoryBean();
+    }
 }
---
+++ src/main/java/com/example/demo/controller/TaskController.java
@@ -0,0 +1,49 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Task;
+import com.example.demo.model.TaskRepository;
+import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController()
+@RequestMapping("/tasks")
+public class TaskController {
+
+    private static final Logger logger = LoggerFactory.getLogger(TaskController.class);
+    private final TaskRepository repository;
+
+    public TaskController(TaskRepository repository) {
+        this.repository = repository;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, params = {"!sort", "!page", "!size"})
+    ResponseEntity<List<Task>> readAllTasks() {
+        logger.warn("Exposing all the tasks");
+        return ResponseEntity.ok(repository.findAll());
+    }
+
+    @GetMapping()
+    ResponseEntity<Page<Task>> readAllTasks(Pageable page) {
+        logger.warn("Custom pageable");
+        return ResponseEntity.ok(repository.findAll(page));
+    }
+
+    @PutMapping("/{id}")
+    ResponseEntity<?> updateTask(@PathVariable("id") int taskId, @RequestBody @Valid Task toUpdate) {
+        if (!repository.existsById(taskId)) {
+            return ResponseEntity.notFound().build();
+        }
+        toUpdate.setId(taskId);
+        repository.save(toUpdate);
+        return ResponseEntity.noContent().build();
+    }
+
+}
