66d370ced376f8cf2473e43f49019f550fe4d990
Piotrek Domagalski <piotr.domagalski@yahoo.com>
2023-02-09 10:52:34 +0100

    creating project group

--- src/main/java/com/example/demo/controller/ProjectController.java
+++ src/main/java/com/example/demo/controller/ProjectController.java
@@ -5,11 +5,13 @@ import com.example.demo.model.Project;
 import com.example.demo.model.ProjectStep;
 import com.example.demo.model.projection.ProjectWriteModel;
 import jakarta.validation.Valid;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Controller
@@ -42,19 +44,36 @@ public class ProjectController {
     }
 
     @PostMapping
-    String addProject(@ModelAttribute("project") @Valid ProjectWriteModel current, BindingResult bindingResult, Model model ){
+    String addProject(@ModelAttribute("project") @Valid ProjectWriteModel current, BindingResult bindingResult, Model model) {
         if (!bindingResult.hasErrors()) {
             projectService.save(current);
             ProjectWriteModel projectWriteModel = new ProjectWriteModel();
             projectWriteModel.setDescription("test from controller");
             model.addAttribute("project", projectWriteModel);
+            model.addAttribute("projects", getProjects());
             model.addAttribute("message", "Projekt został dodany");
         }
         return "projects";
     }
 
+    @PostMapping("/{id}")
+    String createGroup(
+            @ModelAttribute("project") ProjectWriteModel current,
+            Model model,
+            @PathVariable int id,
+            @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm") LocalDateTime deadline) {
+        try {
+            projectService.createGroup(deadline, id);
+            model.addAttribute("message", "Dodano grupę");
+        } catch (Exception e) {
+            model.addAttribute("message", e.getMessage());
+        }
+
+        return "projects";
+    }
+
     @ModelAttribute("projects")
-    List<Project> getProjects(){
-       return projectService.findAll();
+    List<Project> getProjects() {
+        return projectService.findAll();
     }
 }
