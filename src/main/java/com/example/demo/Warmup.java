24c26d7391cac59757e0bf662f25af9f7d0cc657
Piotrek Domagalski <piotr.domagalski@yahoo.com>
2023-02-10 09:27:26 +0100

    ContextRefreshedEvent

--- src/main/java/com/example/demo/Warmup.java
+++ src/main/java/com/example/demo/Warmup.java
@@ -1,11 +1,17 @@
-# Zdarzenia  
+package com.example.demo;
 
-Spring pozwala reagować na różne zdarzenia w ramach działania naszej aplikacji.  
-Aby utowrzyć nowe zdarzenie tworzymy klasę oznaczoną jako ``@Component`` i rozszerzamy interfejs ``ApplicationListener``.
-Interfejs ten jest generyczny i musimy przekazać obiekt rozszerzający ``ApplicationEvent``. 
-  
-Poniżej zdefiniowaliśmy zdarzenie reagujące na utworzenie kontekstu Spring.
-```
+import com.example.demo.model.Task;
+import com.example.demo.model.TaskGroup;
+import com.example.demo.model.TaskGroupRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+
+
 @Component
 public class Warmup implements ApplicationListener<ContextRefreshedEvent> {
     private final static Logger logger = LoggerFactory.getLogger(Warmup.class);
@@ -33,4 +39,3 @@ public class Warmup implements ApplicationListener<ContextRefreshedEvent> {
         }
     }
 }
-```
