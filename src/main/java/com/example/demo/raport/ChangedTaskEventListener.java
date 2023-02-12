23f2b7665d10de61c7d0fdf422de93d13138bf37
Piotrek Domagalski <piotr.domagalski@yahoo.com>
2023-02-12 13:49:40 +0100

    async, section done

--- src/main/java/com/example/demo/raport/ChangedTaskEventListener.java
+++ src/main/java/com/example/demo/raport/ChangedTaskEventListener.java
@@ -5,6 +5,7 @@ import com.example.demo.model.event.TaskUndone;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,12 +19,14 @@ public class ChangedTaskEventListener {
         this.repository = repository;
     }
 
+    @Async
     @EventListener
     public void on(TaskDone event) {
         logger.info("got: " + event);
         repository.save(new PersistedTaskEvent(event));
     }
 
+    @Async
     @EventListener
     public void on(TaskUndone event){
         logger.info("got " + event);
